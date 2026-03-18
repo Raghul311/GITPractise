440d10da0ac80b4144e7e0de8d9263a47253f2c5
Raghul311 <raghulkumar311@gmail.com>
2026-03-18 23:29:28 +0530

    my changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,13 +20,14 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   //timeout: 60000 ,
   expect:{
     timeout:5000,
   },
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -47,7 +48,8 @@ export default defineConfig({
         trace: `retain-on-first-failure`,
     launchOptions: {
       args:["--start-maximized"],
-    }, },
+    }, 
+  },
     },
 
     // {
